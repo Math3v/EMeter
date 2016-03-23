bd4ff4bac418aa8dde32bcbac8661cbd7ef2988b
Matej Minárik <matej.minarik@agile-ict.com>
2016-03-23 07:21:37 +0100

    Show dot indicator

--- main.cpp
+++ main.cpp
@@ -5,43 +5,85 @@
 using namespace std;
 using namespace cv;
 
+#define WIN_TITLE "Electro Meter v0.1"
+#define WIN_W 480
+#define WIN_H 640
+
+void showIndicator(int x, int y, Mat frame) {
+    circle(
+    /* Frame::Mat */        frame, 
+    /* Circle center */     Point(x,y),
+    /* Radius */            40, 
+    /* Scalar(R, G, B) */   Scalar(0, 0, 255), 
+    /* Thickness */         -1, 
+    /* LineType */          8, 
+    /* Shift */             0);
+}
+
 int main(int argc, char** argv ) {
 
     ofstream outfile;
     outfile.open( "data.txt" );
 
-    VideoCapture cap( "./dat/ham_test_2.mp4" );
+    VideoCapture cap( "./dat/br_low_res_1.mp4" );
     if( cap.isOpened() == false ) {
         cerr << "Cannot open file" << endl;
         return -1;
     }
 
-    Mat currFrame;
+    double frameWidth =  cap.get( CV_CAP_PROP_FRAME_WIDTH );
+    double frameHeight = cap.get( CV_CAP_PROP_FRAME_HEIGHT );
+    double videoFPS = cap.get( CV_CAP_PROP_FPS );
+
+    cout << " Width: " << frameWidth << endl;
+    cout << " Height: " << frameHeight << endl;
+    cout << " FPS: " << videoFPS << endl;
+
+    int indicatorX = (int) (WIN_H * 0.15);
+    int indicatorY = (int) (WIN_W * 0.85);
+
+    namedWindow(WIN_TITLE);
+    resizeWindow(WIN_TITLE, WIN_H, WIN_W);
+
+    Mat currFrame, cloneFrame;
     Vec3b currPixel;
     unsigned long sumR, sumG, sumB;
+    unsigned long frameNo = 0;
+    unsigned long lastR = 0;
     while( cap.read(currFrame) ) {
-        int rows = currFrame.rows;
-        int cols = currFrame.cols;
         sumR = sumG = sumB = 0;
 
-        for( int i = 0; i < rows; i++ ) {
-            for( int j = 0; j < cols; j++ ) {
+        cloneFrame = currFrame.clone();
+
+        for( int i = 0; i < frameHeight; i++ ) {
+            for( int j = 0; j < frameWidth; j++ ) {
                 currPixel = currFrame.at<Vec3b>(Point(i, j));
-                sumB += currPixel[0];
-                sumG += currPixel[1];
+                //sumB += currPixel[0];
+                //sumG += currPixel[1];
                 sumR += currPixel[2];
             }
         }
-        
-        double cmsec = cap.get( CV_CAP_PROP_POS_MSEC );
-        //int minutes = (int) ((int) (cmsec / (1000.0*60.0)) % (int) 60);
-        //int seconds = (int) (cmsec / 1000) % 60;
 
-        outfile << cmsec;
+        outfile << frameNo;
         outfile << " " << sumR;
-        outfile << " " << sumG;
-        outfile << " " << sumB;
+        //outfile << " " << sumG;
+        //outfile << " " << sumB;
         outfile << endl;
+
+        ++frameNo;
+        if( lastR != 0 ) {
+            if( (float) sumR / (float) lastR > 1.10 ) {
+                showIndicator(indicatorX, indicatorY, cloneFrame);
+                cout << "Dot " << frameNo << endl;
+            } else {
+                lastR = sumR;
+            }
+        } else {
+            lastR = sumR;
+        }
+
+        imshow(WIN_TITLE, cloneFrame);
+        if( waitKey(29) >= 0 ) break;
     }
 
     outfile.close();
--- plotdata.sh
+++ plotdata.sh
@@ -3,4 +3,4 @@
 #echo "set terminal png large size 4096,4096
 echo "set terminal svg
 set output 'plot.svg' 
-plot 'data.txt' using 1:2 lc rgb 'red' pt 21, '' using 1:3 lc rgb 'green' pt 21, '' using 1:4 lc rgb 'blue' pt 21" | gnuplot
+plot 'data.txt' using 1:2 lc rgb 'red' pt 21" | gnuplot
