eead37d9ac3e78c3c8cfdd9532db0eb4b7e7454a
Matej Minárik <matej.minarik@agile-ict.com>
2016-04-17 13:02:26 +0200

    Add random color filter on dot

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <fstream>
 #include <opencv2/opencv.hpp>
 
+#include <stdlib.h>
+#include <time.h>
+
 using namespace std;
 using namespace cv;
 
@@ -19,9 +22,47 @@ void showIndicator(int x, int y, Mat frame) {
     /* Shift */             0);
 }
 
+int getRandomByte() {
+    return (rand() % 50) + 50;
+}
+
+Vec3b getRandomFilter() {
+    int type = rand() % 6;
+    switch(type) {
+        case 0:
+            return Vec3b(getRandomByte(), 0, 0);
+        case 1:
+            return Vec3b(0, getRandomByte(), 0);
+        case 2:
+            return Vec3b(0, 0, getRandomByte());
+        case 3:
+            return Vec3b(getRandomByte(), getRandomByte(), 0);
+        case 4:
+            return Vec3b(getRandomByte(), 0, getRandomByte());
+        case 5:
+            return Vec3b(0, getRandomByte(), getRandomByte());
+    }
+    return Vec3b(0, 0, 0);
+}
+
+bool filterShouldChange(int frameNo) {
+    static int lastChangeFrameNo = 0;
+    if( 0 == lastChangeFrameNo ){
+        lastChangeFrameNo = frameNo;
+        return true;
+    }
+    else if( frameNo - lastChangeFrameNo > 7 ) {
+        lastChangeFrameNo = frameNo;
+        return true;
+    }
+    return false;
+}
+
 int main(int argc, char** argv ) {
 
     ofstream outfile;
+
+    srand( time(NULL) );
 
     if( argc != 2 ) {
         cerr << "No input file. Usage: " << argv[0] << " VIDEO_CAPTURE_FILE" << endl;
@@ -44,14 +85,15 @@ int main(int argc, char** argv ) {
     cout << " Height: " << frameHeight << endl;
     cout << " FPS: " << videoFPS << endl;
 
-    int indicatorX = (int) ((float) frameHeight * 0.7);
-    int indicatorY = (int) ((float) frameWidth * 0.1);
+    int indicatorY = (int) ((float) frameHeight * 0.1);
+    int indicatorX = (int) ((float) frameWidth * 0.8);
 
     namedWindow(WIN_TITLE);
     resizeWindow(WIN_TITLE, frameHeight, frameWidth);
 
     Mat currFrame, cloneFrame;
     Vec3b currPixel;
+    Vec3b filterPixel;
     unsigned long sumR, sumG, sumB;
     unsigned long frameNo = 0;
     unsigned long lastR = 0;
@@ -62,8 +104,23 @@ int main(int argc, char** argv ) {
 
         for( int i = 0; i < frameHeight; i++ ) {
             for( int j = 0; j < frameWidth; j++ ) {
-                currPixel = currFrame.at<Vec3b>(Point(i, j));
+                currPixel = currFrame.at<Vec3b>(Point(j, i));
                 sumR += currPixel[2];
+                if( cloneFrame.at<Vec3b>(Point(j, i))[0] + filterPixel[0] > 255 ) {
+                    cloneFrame.at<Vec3b>(Point(j, i))[0] = 255;
+                } else {
+                    cloneFrame.at<Vec3b>(Point(j, i))[0] += filterPixel[0];
+                }
+                if( cloneFrame.at<Vec3b>(Point(j, i))[1] + filterPixel[1] > 255 ) {
+                    cloneFrame.at<Vec3b>(Point(j, i))[1] = 255;
+                } else {
+                    cloneFrame.at<Vec3b>(Point(j, i))[1] += filterPixel[1];
+                }
+                if( cloneFrame.at<Vec3b>(Point(j, i))[2] + filterPixel[2] > 255 ) {
+                    cloneFrame.at<Vec3b>(Point(j, i))[2] = 255;
+                } else {
+                    cloneFrame.at<Vec3b>(Point(j, i))[2] += filterPixel[2];
+                }
             }
         }
 
@@ -75,7 +132,10 @@ int main(int argc, char** argv ) {
         if( lastR != 0 ) {
             if( (float) sumR / (float) lastR > 1.10 ) {
                 showIndicator(indicatorX, indicatorY, cloneFrame);
-                cout << "Dot " << frameNo << endl;
+                cout << "Dot " << frameNo << " " << indicatorX << ":" << indicatorY << endl;
+                if( true == filterShouldChange(frameNo) ) {
+                    filterPixel = getRandomFilter();
+                }
             } else {
                 lastR = sumR;
             }
